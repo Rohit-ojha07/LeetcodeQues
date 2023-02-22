a9bf2c0223252662280ad8418c79a60a2e6f492d
Rohit <78247571+Rohit-ojha07@users.noreply.github.com>
2023-02-22 23:24:46 +0530

    Added solution - LeetHub

--- Pattern - GFG/pattern.java
+++ Pattern - GFG/pattern.java
@@ -26,15 +26,18 @@ class GFG {
 // User function Template for Java
 
 class Solution {
-
     void printDiamond(int n) {
         // Your code here
         for(int i=0;i<n;i++){
-            for(int j=0;j<n-i-1;j++){
-               System.out.print(" ");
-            }for(int j=0;j<=i;j++){
-                System.out.print("* ");
+            for(int j=n-1-i;j>0;j--){
+                System.out.print(" ");
             }
+            for(int j=0;j<=i;j++){
+                System.out.print("*"+" ");
+            }
+            // for(int j=0;j<1;j++){
+            //     System.out.print(" ");
+            // }
             System.out.println();
         }
         for(int i=0;i<n;i++){
@@ -42,7 +45,7 @@ class Solution {
                 System.out.print(" ");
             }
             for(int j=n;j>i;j--){
-                System.out.print("* ");
+                System.out.print("*"+" ");
             }
             System.out.println();
         }
